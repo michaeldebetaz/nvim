9c75a107e96111cdcf25814280006bd54dcaf465
michaeldebetaz <netarion@outlook.com>
2023-08-06 09:52:20 +0200

    Remove eslint_d

--- init.lua
+++ init.lua
@@ -218,7 +218,7 @@ require('lazy').setup({
         sources = {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.prettier,
-          null_ls.builtins.diagnostics.eslint_d,
+          null_ls.builtins.diagnostics.eslint,
         },
         on_attach = function(client, bufnr)
           if client.supports_method("textDocument/formatting") then
