0454892734b7f93b99ee14d682597b093234940b
michaeldebetaz <netarion@outlook.com>
2023-12-06 00:08:57 +0100

    Remove useless autocmd

--- init.lua
+++ init.lua
@@ -109,15 +109,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    require("go.format").goimport()
-  end,
-  group = format_sync_grp,
-})
-
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 require("telescope").setup({
