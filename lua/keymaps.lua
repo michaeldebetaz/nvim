b03cb5030e79b074eceeb9b658308d906b023fd1
michaeldebetaz <netarion@outlook.com>
2024-01-02 23:32:50 +0100

    Add move lines keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,15 +19,23 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("n", "<F2>", ":Neotree toggle<CR>", { silent = true })
 
+-- Move lines
+vim.keymap.set("n", "<A-j>", ":move .+1<CR>", { silent = true })
+vim.keymap.set("n", "<A-k>", ":move .-2<CR>", { silent = true })
+vim.keymap.set("i", "<A-j>", "<Esc>:move .+1<CR>==gi", { silent = true })
+vim.keymap.set("i", "<A-k>", "<Esc>:move .-2<CR>==gi", { silent = true })
+vim.keymap.set("v", "<A-j>", ":move '>+1<CR>gv=gv", { silent = true })
+vim.keymap.set("v", "<A-k>", ":move '<-2<CR>gv=gv", { silent = true })
+
 -- See `:help telescope.builtin`
 vim.keymap.set("n", "<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
 vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[ ] Find existing buffers" })
 vim.keymap.set("n", "<leader>/", function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-    winblend = 10,
-    previewer = false,
-  }))
+	-- You can pass additional configuration to telescope to change theme, layout, etc.
+	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+		winblend = 10,
+		previewer = false,
+	}))
 end, { desc = "[/] Fuzzily search in current buffer" })
 vim.keymap.set("n", "<leader>gf", require("telescope.builtin").git_files, { desc = "Search [G]it [F]iles" })
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
