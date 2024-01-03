bca5e84f9b10e4f9682c089c0ff8342156971fbc
michaeldebetaz <netarion@outlook.com>
2024-01-03 02:03:13 +0100

    Fix copilot plugin configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,6 +4,19 @@ return {
 	cmd = "Copilot",
 	event = "InsertEnter",
 	config = function()
-		require("copilot").setup({})
+		require("copilot").setup({
+			suggestion = {
+				auto_trigger = true,
+				keymap = {
+					accept = "<Tab>",
+					accept_word = false,
+					accept_line = false,
+					next = "<C-]>",
+					prev = "<C-[>",
+					dismiss = "<A-0>",
+				},
+			},
+			copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v20.10.0/bin/node", -- Node.js version must be > 18.x
+		})
 	end,
 }
