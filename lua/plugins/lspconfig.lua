9cc917dfe54ad15b69d2f3de6509c3ab78dfbc13
michaeldebetaz <netarion@outlook.com>
2024-03-13 22:55:31 +0100

    feat: update following kickstart.nvim

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -116,21 +116,7 @@ return {
 			},
 			lua_ls = {
 				settings = {
-					Lua = {
-						runtime = { version = "LuaJIT" },
-						workspace = {
-							checkThirdParty = false,
-							-- Tells lua_ls where to find all the Lua files that you have loaded
-							-- for your neovim configuration.
-							library = {
-								"${3rd}/luv/library",
-								unpack(vim.api.nvim_get_runtime_file("", true)),
-							},
-							-- If lua_ls is really slow on your computer, you can try this instead:
-							-- library = { vim.env.VIMRUNTIME },
-						},
-						completion = { callSnippet = "Replace" },
-					},
+					Lua = { completion = { callSnippet = "Replace" } },
 				},
 			},
 			pyright = {},
